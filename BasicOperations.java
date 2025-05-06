fb37867d81cdfd345a213598fe94c2d36d480c00
satwik1111 <naikn5637@gmail.com>
2025-05-06 16:07:30 +0530

    save file

---
+++ BasicOperations.java
@@ -0,0 +1,12 @@
+public class BasicOperations {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 5;
+        int sum = a + b;
+        int diff = a - b;
+        int prod = a * b;
+        System.out.println("Addition: " + sum);
+        System.out.println("Substraction: " + diff);
+        System.out.println("Multiplication: " + prod);
+ }
+}
